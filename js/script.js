1694d8b2297552bb45e8d950845ff75aaf740e62
sori-sodong <sorisa4@naver.com>
2022-04-12 01:18:07 +0900

    자바스크립트를 활용한 미니 퀴즈

---
+++ js/script.js
@@ -0,0 +1,64 @@
+const questionCount = document.querySelector('.question-p');
+const question = document.querySelector('.question');
+const choice = document.querySelector('.choices');
+
+let questionNumber = 1;
+let score = 0;
+
+function updatePage() {
+  if (questionNumber < 5) {
+
+    questionCount.innerText = `Question ${questionNumber}/4`;  
+    const questions = [
+      'What is 1 + 1',
+      'What is 2 + 3',
+      'What is 3 + 7',
+      'What is 9 + 9'
+    ];
+    let childNum = 0;
+    const choiceAlphabet = ['A', 'B', 'C', 'D'];
+    const answerSheet = [[2,4,5,7],[1,5,10,11], [20,30,10,14],[12,20,27,18]];
+    
+    question.innerText = `${questions[questionNumber-1]}`;
+    const choices = document.querySelector('.choices');
+    choices.innerHTML = ''
+    for(let i = 0; i < 4; i++) {
+      choices.insertAdjacentHTML('beforeend', 
+      `<div class = "child${i+1}">
+      <div class="child-inner">${choiceAlphabet[i]}</div>
+      ${answerSheet[questionNumber-1][i]}
+      </div>
+      `);
+    }
+    const child = document.querySelectorAll('[class^=child]');
+    for (let c of child) {
+      c.addEventListener('click', () => {
+        let val = c.innerText;
+        checkAnswer(questionNumber, val[0]);
+      })
+    }
+    document.querySelector('.score').innerText = score;
+  } else {
+    const body = document.querySelector('body');
+    body.innerHTML = '';
+    body.insertAdjacentHTML('afterbegin', `
+    <h1 class=result style="color: #5AA7EB;">Total score: ${score}</h1>
+    <a href="quiz.html">
+    <button class="start-btn">Play Again</button>
+    </a>
+    `);
+    
+  }
+}
+
+
+
+function checkAnswer(question, answer) {
+  document.querySelector('.meter').value += 25;
+  if((question === 1 && answer === 'A') || (question === 2 && answer === 'B') || (question === 3 && answer === 'C') || (question === 4 && answer === 'D')) {
+    score++;
+  } 
+  questionNumber++;
+  updatePage();
+}
+
